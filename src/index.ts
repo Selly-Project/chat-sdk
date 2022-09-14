506191efa841b9124fa8f65cb6fd6cf6f550b741
votantai <taivo@selly.vn>
2022-09-14 15:42:31 +0700

    function init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
-export function greet(name: string): string {
-  return `Hello ${name}`
+type SourceType = 'supplier_tool' | 'selly_admin';
+
+interface InitProps {
+  userToken: string;
+  fcmToken: string;
+  deviceId: string;
+  source: SourceType;
+}
+
+export class SellyChat {
+  private setupChat(params: InitProps) {
+  console.log(`Func: setupChat - PARAMS: params`, params);
+    const { userToken, fcmToken, deviceId, source } = params;
+  }
+
+  public init(params: InitProps) {
+    this.setupChat(params);
+  }
+
+  public destroy() {}
+
+  public getUnreadMessage() {}
 }
